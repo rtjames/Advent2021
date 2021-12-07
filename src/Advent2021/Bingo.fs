a8d43626cf25bddf2cd3af905d8e79932f3863dc
robert.james <robert.james@blueskydevops.net>
2021-12-07 16:42:01 -0500

    Started day 4, part 1. wip

---
+++ src/Advent2021/Bingo.fs
@@ -0,0 +1,40 @@
+﻿namespace Advent2021
+
+type BingoCell =
+    {
+        Number: int
+        IsMarked: bool
+    }
+
+
+module Bingo =
+
+    let createCell (num: int) =
+        { Number = num; IsMarked = false }
+
+    let markCell (cell: BingoCell) =
+        { cell with IsMarked = true; }
+
+    let createBoard (nums: int [,]) =
+        let initialize i j =
+            nums.[i,j] |> createCell
+        Array2D.init 5 5 initialize
+
+    let markBoard (num: int) (board: BingoCell [,]) =
+        let markIfNum (cell: BingoCell) =
+            if cell.Number = num then (markCell cell)
+            else cell
+        Array2D.map markIfNum board
+
+    let hasWon (board: BingoCell [,]) =
+        let checkRowColum (index: int) =
+            let row = board.[index, *]
+            let rowWins = Array.forall (fun cell -> cell.IsMarked) row
+            if rowWins then true
+            else
+                let column = board.[*, index]
+                Array.forall (fun cell -> cell.IsMarked) column
+        let checkDiagonal (i: int) =
+            if board.[i,i].IsMarked then checkRowColum i 
+            else false
+        seq {0..4} |> Seq.exists checkDiagonal
--- src/Advent2021/Program.fs
+++ src/Advent2021/Program.fs
@@ -5,7 +5,25 @@ module Program =
 
     [<EntryPoint>]
     let main argv =
-
+        let test = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+        
+        22 13 17 11  0
+         8  2 23  4 24
+        21  9 14 16  7
+         6 10  3 18  5
+         1 12 20 15 19
+        
+         3 15  0  2 22
+         9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6
+        
+        14 21 17 24  4
+        10 16 15  9 19
+        18  8 23 26 20
+        22 11 13  6  5
+         2  0 12  3  7"""
 
         let days = 
             [|
@@ -56,6 +74,36 @@ module Program =
                         let codes = lines |> Array.toList |> List.map DiagnosticCode.Create
                         let lifeSupportRating = DiagnosticCode.CalculateLifeSupportRating codes
                         sprintf "%i" lifeSupportRating //1613181
+                };
+                {
+                    Date = Day.createDate 4 12 2021
+                    SolvePart1 = fun input ->
+                        let lines = input.Split("\n\r\n")
+                        let numbers = lines.[0].Split(',') |> Array.map Int32.Parse |> List.ofArray
+                        let create2dArrayFromBoardStr (boardStr: string) =
+                            let s = boardStr.Trim()
+                            let sSplit = s.Split("\r\n") 
+                            sSplit 
+                            |> Array.map (fun str -> str.Split(' ') |> Array.filter (fun x -> not (x = ""))) 
+                            |> array2D 
+                            |> Array2D.map Int32.Parse 
+                        let boards = 
+                            lines.[1..] 
+                            |> List.ofArray 
+                            |> List.map create2dArrayFromBoardStr 
+                            |> List.map Bingo.createBoard
+                        let findWinner =
+                                let rec checkBoards (nums: int list) (boards: BingoCell[,] list) =
+                                    let markedBoards = List.map (Bingo.markBoard nums.Head) boards
+                                    match List.tryFind Bingo.hasWon markedBoards with
+                                    | Some b -> (nums.Head, b)
+                                    | _ -> checkBoards nums.Tail markedBoards
+
+                                checkBoards numbers boards
+                        findWinner |> ignore
+                        ""
+                    SolvePart2 = fun input ->
+                        ""
                 }
             |]
         
